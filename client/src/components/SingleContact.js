8750574629c20be29573f2fe653596d3882fa32a
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-08 23:50:31 -0600

    Create SingleContact.js
    - Created a component to display contact details on card

---
+++ client/src/components/SingleContact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SingleContact = ({contact}) => (
+    <div className="row">
+    <div className="col s12 m6">
+      <div className="card blue-grey darken-1">
+        <div className="card-content white-text">
+            <span className="card-title">{contact.firstName} {contact.lastName}</span>
+        </div>
+        <div className="card-action">
+            <p>{contact.email}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default SingleContact;
